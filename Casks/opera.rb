e3c3ac58b52b4eb8f8bd631ed2f89c89b5d8426d
UltraHDR <108294295+UltraHDR@users.noreply.github.com>
2022-07-21 19:49:47 +0100

    Update opera.rb

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -2,7 +2,7 @@ cask "opera" do
   version "89.0.4447.51"
   sha256 "92f62abf5adcd16fec123ac6dd3c95513ebf3e5d4778c76eac7a8bef848e20c2"
 
-  url "https://download1.operacdn.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
+  url "https://ftp.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name "Opera"
   desc "Web browser"
   homepage "https://www.opera.com/"
